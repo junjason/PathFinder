ac8451c70edc3f0e552b7a46602989e47bedbe4a
Jason Jun <junjason@umich.edu>
2023-10-24 15:18:53 -0400

    added portals front-end logic, need to add logic to bfs, dfs, and backtrack

--- src/index.js
+++ src/index.js
@@ -8,29 +8,97 @@ document.addEventListener("DOMContentLoaded", () => {
     const gridContainer = document.querySelector(".grid-container");
     grid = new Grid(gridContainer);
 
-    let visualizeBFS = document.querySelector(".visualizeBFS");
-    let visualizeDFS = document.querySelector(".visualizeDFS");
-
-    visualizeBFS.addEventListener("click", () => {
-        let start = document.querySelector(".start");
-        let end = document.querySelector(".end");
-        let route = new Route(grid, start, end);
-        let pathFound = route.runBFS();
-        if (pathFound) route.backTrack();
-    });
-
-    visualizeDFS.addEventListener("click", () => {
-        let start = document.querySelector(".start");
-        let end = document.querySelector(".end");
-        let route = new Route(grid, start, end);
-        let pathFound = route.runDFS();
-        if (pathFound) route.backTrack();
-    });
-
     let clearBoard = document.querySelector(".resetBoard")
     clearBoard.addEventListener("click", () => {
         grid.resetBoard();
     })
+
+
+    // Drop down functionality for algorithms
+    let dropbtn = document.querySelector(".dropbtn");
+    let dropdownContent = document.getElementById("myDropdown");
+
+    dropbtn.addEventListener("click", function() {
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    });
+
+    // algo selection logic
+    let algo = null;
+    let visualize = document.querySelector(".visualize");
+    visualize.addEventListener("click", () => {
+        if (algo === null) {
+            visualize.innerText = "Select an algorithm!";
+        }
+        else if (algo === "bfs") {
+            let start = document.querySelector(".start");
+            let end = document.querySelector(".end");
+            let route = new Route(grid, start, end);
+            let pathFound = route.runBFS();
+            if (pathFound) route.backTrack();
+        }
+        else if (algo === "dfs") {
+            let start = document.querySelector(".start");
+            let end = document.querySelector(".end");
+            let route = new Route(grid, start, end);
+            let pathFound = route.runDFS();
+            if (pathFound) route.backTrack();
+        }
+    });
+
+    let selectedBFS = document.querySelector(".selectedBFS");
+    let selectedDFS = document.querySelector(".selectedDFS");
+    selectedBFS.addEventListener("click", () => {
+        algo = "bfs";
+        visualize.innerText = "Visualize";
+        dropdownContent.style.display = "none";
+    });
+
+    selectedDFS.addEventListener("click", () => {
+        algo = "dfs";
+        visualize.innerText = "Visualize";
+        dropdownContent.style.display = "none";
+    });
+
+    // place portals event handler
+    let portalBtn = document.querySelector(".placePortals");
+    let portalExists = false;
+    portalBtn.addEventListener("click", () => {
+        if (!portalExists) {
+            portalExists = true;
+            grid.placePortals();
+            portalBtn.innerHTML = "Remove Portals";
+        }
+        else if (portalExists){
+            portalExists = false;
+            grid.removePortals();
+            portalBtn.innerHTML = "Place Portals";
+        }
+    });
+
+
+    // Drop down functionality for auto generate walls
+    let dropWallbtn = document.getElementById("wallDropBtn");
+    let wallDropDownContent = document.querySelector(".auto-wall-dropdown");
+    dropWallbtn.addEventListener("click", () => {
+        if (wallDropDownContent.style.display === "block") {
+            wallDropDownContent.style.display = "none";
+        } else {
+            wallDropDownContent.style.display = "block";
+        }
+    });
+
+    // logic for auto generate walls
+
+
+    
+
+
+
+
 });
 
 
--- src/scripts/grid.js
+++ src/scripts/grid.js
@@ -18,6 +18,7 @@ class Grid {
                 node.classList.add(`n-${j}-${k}`);
                 node.classList.add("node");
                 node.dataset.status = "unvisited";
+                node.dataset.portal = "none";
                 node.draggable = false;
                 node.addEventListener("click", () => {
                     if (node.dataset.status === "wall") {
@@ -72,7 +73,7 @@ class Grid {
             const data = event.dataTransfer.getData("text");
             if (data === "start") {
                 const targetNode = event.target;
-                if (!targetNode.classList.contains("node") || targetNode.dataset.status === "wall") return;
+                if (!targetNode.classList.contains("node") || targetNode.dataset.status !== "unvisited") return;
                 targetNode.innerHTML = start.innerHTML; // Update the icon visually
                 targetNode.classList.add("start");
                 targetNode.dataset.status = "start";
@@ -84,13 +85,13 @@ class Grid {
                 start = document.querySelector(".start"); // reset start variable so it can be drag and dropped again
             } else if (data === "end") {
                 const targetNode = event.target;
-                if (!targetNode.classList.contains("node") || targetNode.dataset.status === "wall") return;
+                if (!targetNode.classList.contains("node") || targetNode.dataset.status !== "unvisited") return;
                 targetNode.innerHTML = end.innerHTML; // Update the icon visually
                 targetNode.classList.add("end");
                 targetNode.dataset.status = "end";
                 targetNode.draggable = true; // Set draggable to true again
-                end.classList.remove("end");
                 end.innerHTML = ""; // Clear the previous icon
+                end.classList.remove("end");
                 end.dataset.status = "unvisited";
                 end.draggable = false;
                 end = document.querySelector(".end");   // reset end variable so it can be drag and dropped again
@@ -106,6 +107,84 @@ class Grid {
         this.initializeStartAndEnd();
     }
 
+
+    placePortals() {
+        let startP = document.querySelector(".n-11-14");
+        let endP = document.querySelector(".n-11-30");
+
+        startP.classList.add("portal-start");
+        endP.classList.add("portal-end");
+        startP.dataset.portal = "portal-start";
+        endP.dataset.portal = "portal-end";
+
+        // Make the icons draggable
+        startP.draggable = true;
+        endP.draggable = true;
+
+        startP.innerHTML = "<i class=\"fa-solid fa-door-open\"></i>";
+        endP.innerHTML = "<i class=\"fa-solid fa-door-open\"></i>";
+
+        // Add dragstart event listener for the icons
+        this.gridContainer.addEventListener("dragstart", (event) => {
+            const targetElement = event.target;
+            if (targetElement === startP) {
+                event.dataTransfer.setData("text", "portal-start");
+            } else if (targetElement === endP) {
+                event.dataTransfer.setData("text", "portal-end");
+            }
+        });
+
+        // Add drop event listener for the grid cells
+        this.gridContainer.addEventListener("dragover", (event) => {
+            event.preventDefault();
+        });
+
+        this.gridContainer.addEventListener("drop", (event) => {
+            event.preventDefault();
+            const data = event.dataTransfer.getData("text");
+            if (data === "portal-start") {
+                const targetNode = event.target;
+                if (!targetNode.classList.contains("node") || targetNode.dataset.status !== "unvisited") return;
+                targetNode.innerHTML = startP.innerHTML; // Update the icon visually
+                targetNode.classList.add("portal-start");
+                targetNode.dataset.portal = "portal-start";
+                targetNode.draggable = true; // Set draggable to true again
+                startP.innerHTML = ""; // Clear the previous icon
+                startP.classList.remove("portal-start");
+                startP.dataset.portal = "none";
+                startP.draggable = false;
+                startP = document.querySelector(".portal-start"); // reset start variable so it can be drag and dropped again
+            } else if (data === "portal-end") {
+                const targetNode = event.target;
+                if (!targetNode.classList.contains("node") || targetNode.dataset.status !== "unvisited") return;
+                targetNode.innerHTML = endP.innerHTML; // Update the icon visually
+                targetNode.classList.add("portal-end");
+                targetNode.dataset.portal = "portal-end";
+                targetNode.draggable = true; // Set draggable to true again
+                endP.innerHTML = ""; // Clear the previous icon
+                endP.classList.remove("portal-end");
+                endP.dataset.portal = "none";
+                endP.draggable = false;
+                endP = document.querySelector(".portal-end");   // reset end variable so it can be drag and dropped again
+            }
+        });
+    }
+
+    removePortals() {
+        let startP = document.querySelector(".portal-start");
+        let endP = document.querySelector(".portal-end");
+
+        startP.classList.remove("portal-start");
+        endP.classList.remove("portal-end");
+        startP.dataset.portal = "none";
+        endP.dataset.portal = "none";
+
+        startP.draggable = false;
+        endP.draggable = false;
+
+        startP.innerHTML = "";
+        endP.innerHTML = "";
+    }
     
 }
 
